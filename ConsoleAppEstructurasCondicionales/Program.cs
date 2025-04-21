874c57d655da24ebcb16db909d2a53587c036983
AlexanderTituaña <bryan.crew11@gmail.com>
2025-04-20 22:34:12 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleAppEstructurasCondicionales/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        int edad = 22;
+
+
+
+        if (edad >= 18)
+
+            Console.WriteLine("Es adulto");
+
+        else
+
+            Console.WriteLine("Es menor");
+
+
+
+        Console.ReadKey();
+
+    }
+
+}
